6fe2b8090218b84b486a7bd364c3ab35db6750e1
Tobias Deißler <tobias_christian.deissler@student.reutlingen-university.de>
2018-03-03 00:52:15 +0100

    Improved layout. List content loading not working.

--- listLoader.js
+++ listLoader.js
@@ -1,14 +1,37 @@
 $(function(){
+	console.log("Starting lists.json request.");
+	var listsFileLoader = new XMLHttpRequest();
+	listsFileLoader.onload = parseListsJson;
+    listsFileLoader.open("GET", "lists.json");
+	console.log("lists.json request started.");
+	
+	$("#listCollectionContainer").load("list.html", loadListContent);
 	$("#listCollectionContainer").load("list.html", loadListContent);
 });
+
+function parseListsJson()
+{
+	console.log("Parsing lists.");
+	var listContainer = JSON.parse(this.responseText); 
+	listContainer.lists.forEach(function(list)
+	{
+		console.log(list.name);
+	});
+}
 
 function loadListContent()
 {
 	$(".listElementContainer").load("list_element.html", loadListElementContent);
+	
+	// test content
+	var testListName = "Lista 1"
+	$(".listTitleText").prop("id", testListName);
+	$(".listTitleText").text(testListName);
 }
 
 function loadListElementContent()
 {
-	$(".listElement > .listElementName").text("Elemento 1");
-	$(".listElement > .listElementQuantity").text("3 ud.");
+	// test content
+	$(".listElementName").text("Elemento 1");
+	$(".listElementQuantity").text("3 ud.");
 }
